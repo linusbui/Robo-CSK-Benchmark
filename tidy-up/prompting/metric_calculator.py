b4c0d4a9c5c42c6cd260d5ed833361b8b46c4ef9
Janfiderheld <Janfitoe@web.de>
2024-07-23 14:28:37 +0200

    calculate recall@k

--- tidy-up/prompting/metric_calculator.py
+++ tidy-up/prompting/metric_calculator.py
@@ -28,6 +28,16 @@ def calculate_precision_at_k(k: int, retrieved: [str], gold_standard: [str]) -> 
     return c / k
 
 
+def calculate_recall_at_k(k: int, retrieved: [str], gold_standard: [str]) -> float:
+    assert k >= 1
+    assert k <= len(gold_standard)
+    c = 0
+    for r in retrieved[0:k]:
+        if r in gold_standard:
+            c += 1
+    return c / len(gold_standard)
+
+
 def calculate_reciprocal_rank(retrieved: [str], gold_standard: [str]) -> float:
     for r in retrieved:
         if r in gold_standard:
