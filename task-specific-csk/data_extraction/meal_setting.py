2c415d7021a578eb1f3e28e9fe8349ba1a53f50c
Janfiderheld <Janfitoe@web.de>
2024-11-13 11:05:26 +0100

    adapt utensil representation and wwd extraction accordingly

--- task-specific-csk/data_extraction/meal_setting.py
+++ task-specific-csk/data_extraction/meal_setting.py
@@ -1,44 +1,31 @@
 class MealSetting:
-    def __init__(self, meal: str, fork: bool, spoon: bool, knife: bool, plate: str):
+    def __init__(self, meal: str, utensils: [str], plate: str):
         self._meal = meal
-        self._needs_fork = fork
-        self._needs_spoon = spoon
-        self._needs_knife = knife
+        self._utensils = utensils
         self._plate_type = plate
 
     def __str__(self):
-        utensils = []
-        if self._needs_fork:
-            utensils.append('Fork')
-        if self._needs_spoon:
-            utensils.append('Spoon')
-        if self._needs_knife:
-            utensils.append('Knife')
-        if len(utensils) == 0:
-            utensils.append('Hands')
-        return f'To eat a {self._meal} a person needs a {self._plate_type} and the following utensils: {utensils}'
+        ut_str = str(self._utensils)
+        if len(self._utensils) == 0:
+            ut_str = 'Hands'
+        return f'To eat a {self._meal} a person needs a {self._plate_type} and the following utensils: {ut_str}'
 
     def get_meal(self) -> str:
         return self._meal
 
-    def needs_fork(self) -> bool:
-        return self._needs_fork
-
-    def needs_spoon(self) -> int:
-        return self._needs_spoon
-
-    def needs_knife(self) -> int:
-        return self._needs_knife
+    def get_utensils(self) -> [str]:
+        return self._utensils
 
     def get_plate(self) -> str:
         return self._plate_type
 
+    def add_utensil(self, utensil: str):
+        self._utensils.append(utensil)
+
     def to_dict(self):
         return {
             'Meal': self.get_meal(),
-            'Fork?': self.needs_fork(),
-            'Spoon?': self.needs_spoon(),
-            'Knife?': self.needs_knife(),
-            'Hands?': not self.needs_fork() and not self.needs_spoon() and not self._needs_knife,
+            'Utensils': self.get_utensils(),
+            'Hands?': len(self.get_utensils()) == 0,
             'Plate Type': self.get_plate()
         }
--- task-specific-csk/data_extraction/wwd_extractor.py
+++ task-specific-csk/data_extraction/wwd_extractor.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from numpy.matlib import empty
 
 from meal_setting import MealSetting
 
@@ -12,25 +11,57 @@ def get_meal_settings_from_wwd() -> [MealSetting]:
         meal_name = str(row['english_name'])
         if meal_name == "nan":
             continue
-        utensils = str(row['utensils'])
-        if utensils == "nan":
+        uts = str(row['utensils'])
+        if uts == "nan":
             continue
         plate = _get_plate_type_from_dish_type(row['type_of_dish'])
-        setting = MealSetting(meal_name, 'fork' in utensils, 'spoon' in utensils, 'knife' in utensils, plate)
+        utensils = _process_utensils(uts)
+        setting = MealSetting(meal_name, utensils, plate)
         res.append(setting)
     return res
 
 
 def _get_plate_type_from_dish_type(dish_type: str) -> str:
-    if 'Main dish' in dish_type:
-        return 'Normal Plate'
+    if 'Soup' in dish_type or 'Sauce' in dish_type or 'Salad' in dish_type:
+        return 'Bowl'
     if 'Dessert' in dish_type or 'Side dish' in dish_type or 'Starter' in dish_type:
         return 'Small Plate'
     if 'Small plate / bowl for sharing' in dish_type:
         return 'Small Plate or Bowl'
-    if 'Soup' in dish_type or 'Sauce' in dish_type or 'Salad' in dish_type:
-        return 'Bowl'
+    if 'Main dish' in dish_type:
+        return 'Normal Plate'
 
+
+def analyse_utensils() -> [str]:
+    data = pd.read_csv("../data/WorldWideDishes_2024_June.csv", delimiter=',', on_bad_lines='skip')
+    uts = {}
+
+    for index, row in data.iterrows():
+        utensils = str(row['utensils'])
+        if utensils == "nan":
+            continue
+        for ut in utensils.split(","):
+            ut_fil = ut.strip()
+            if ut_fil not in uts:
+                uts[ut_fil] = 1
+            else:
+                uts[ut_fil] += 1
+    print(uts)
+
+
+def _process_utensils(utensils: [str]) -> [str]:
+    result = []
+    for ut in utensils.split(","):
+        ut_proc = ut.strip().lower()
+        if "fork" in ut_proc:
+            result.append("fork")
+        if "knife" in ut_proc:
+            result.append("knife")
+        if "spoon" in ut_proc:
+            result.append("spoon")
+        if "chopstick" in ut_proc:
+            result.append("chopsticks")
+    return result
 
 if __name__ == '__main__':
     res = get_meal_settings_from_wwd()
