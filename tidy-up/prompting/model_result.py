bbe3e98da5cbaa7ae17367fb8a8a163dceee7d49
Janfiderheld <Janfitoe@web.de>
2025-01-15 13:40:57 +0100

    add prompting for tidy uo task

---
+++ tidy-up/prompting/model_result.py
@@ -0,0 +1,77 @@
+from metric_calculator import calculate_reciprocal_rank, calculate_average_precision_at_k, calculate_recall_at_k
+
+
+class ModelResultTuple:
+    def __init__(self, obj: str, pred_locations: [str], corr_locations: [str]):
+        self._object = obj
+        self._pred_locations = []
+        self._pred_locations.extend(pred_locations)
+        self._corr_locations = []
+        self._corr_locations.extend(corr_locations)
+        self._rr = 0.0
+        self._ap5 = 0.0
+        self._ap3 = 0.0
+        self._ap1 = 0.0
+        self._rec5 = 0.0
+        self._rec3 = 0.0
+        self._rec1 = 0.0
+        self.calculate_metrics()
+
+    def __str__(self):
+        return (f'{self._object} located at/in {self._pred_locations} with RR: {self._rr:.2f}, AP@1: {self._ap1:.2f}, '
+                f'AP@3: {self._ap3:.2f}, AP@5: {self._ap5:.2f}, Rec@1: {self._rec1:.2f}, Rec@3: {self._rec3:.2f}, '
+                f'Rec@5: {self._rec5:.2f}')
+
+    def get_object(self) -> str:
+        return self._object
+
+    def get_predicted_locations(self) -> [str]:
+        return self._pred_locations
+
+    def get_correct_locations(self) -> [str]:
+        return self._corr_locations
+
+    def get_reciprocal_rank(self) -> float:
+        return self._rr
+
+    def get_average_precision_at1(self) -> float:
+        return self._ap1
+
+    def get_average_precision_at3(self) -> float:
+        return self._ap3
+
+    def get_average_precision_at5(self) -> float:
+        return self._ap5
+
+    def get_recall_at1(self) -> float:
+        return self._rec1
+
+    def get_recall_at3(self) -> float:
+        return self._rec3
+
+    def get_recall_at5(self) -> float:
+        return self._rec5
+
+    def calculate_metrics(self):
+        if len(self._corr_locations) == 0:
+            return
+        self._rr = calculate_reciprocal_rank(self._pred_locations, self._corr_locations)
+        self._ap1 = calculate_average_precision_at_k(1, self._pred_locations, self._corr_locations)
+        self._ap3 = calculate_average_precision_at_k(3, self._pred_locations, self._corr_locations)
+        self._ap5 = calculate_average_precision_at_k(5, self._pred_locations, self._corr_locations)
+        self._rec1 = calculate_recall_at_k(1, self._pred_locations, self._corr_locations)
+        self._rec3 = calculate_recall_at_k(3, self._pred_locations, self._corr_locations)
+        self._rec5 = calculate_recall_at_k(5, self._pred_locations, self._corr_locations)
+
+    def to_dict(self):
+        return {
+            'Object': self.get_object(),
+            'Predicted Locations': self.get_predicted_locations(),
+            'Reciprocal Rank': self.get_reciprocal_rank(),
+            'Average Precision@1': self.get_average_precision_at1(),
+            'Average Precision@3': self.get_average_precision_at3(),
+            'Average Precision@5': self.get_average_precision_at5(),
+            'Recall@1': self.get_recall_at1(),
+            'Recall@3': self.get_recall_at3(),
+            'Recall@5': self.get_recall_at5()
+        }
---
+++ tidy-up/prompting/tidy_up_prompter.py
@@ -0,0 +1,59 @@
+import ast
+
+import pandas as pd
+
+from model_result import ModelResultTuple
+from utils import OpenAIPrompter, LlamaPrompter, GemmaPrompter
+
+prompters = [OpenAIPrompter(), LlamaPrompter(), GemmaPrompter()]
+system_msg = 'Imagine you are a robot tidying up a household.'
+user_msg = 'What are the prototypical locations in a household where the following object can be found? Please only answer with a comma separated & ranked list of locations.'
+
+
+def prompt_all_models():
+    comb_result = pd.DataFrame(columns=['model', 'rr', 'ap@1', 'ap@3', 'ap@5', 'rec@1', 'rec@3', 'rec@5'])
+    for prompter in prompters:
+        data = pd.read_csv('../tidy_up_data.csv', delimiter=',', on_bad_lines='skip')
+        results = []
+        for index, row in data.iterrows():
+            obj = row['Object']
+            locs = row['Locations']
+            question = f'Object: {obj}\nLocations:'
+            res = prompter.prompt_model(system_msg, user_msg, question)
+            tup = ModelResultTuple(obj, format_generated_locations(res), format_gold_standard_locations(locs))
+            results.append(tup)
+        write_results_to_file(results, prompter.model_name)
+        comb_result = pd.concat([comb_result, calculate_average(results, prompter.model_name)], ignore_index=True)
+    comb_result.to_csv('../results/model_overview.csv', index=False)
+
+
+def write_results_to_file(results: [ModelResultTuple], model: str):
+    dict_list = [re.to_dict() for re in results]
+    df = pd.DataFrame(dict_list)
+    df.to_csv(f'../results/{model}.csv', index=False)
+
+
+def format_generated_locations(locations: str) -> [str]:
+    return locations.lower().split(',')
+
+
+def format_gold_standard_locations(locations) -> [str]:
+    data_dict = ast.literal_eval(locations)
+    return [value[0].lower() for value in data_dict.values()]
+
+
+def calculate_average(results: [ModelResultTuple], model: str):
+    average = {met: 0 for met in ['rr', 'ap@1', 'ap@3', 'ap@5', 'rec@1', 'reca@3', 'rec@5']}
+    for res in results:
+        average['rr'] += res.get_reciprocal_rank()
+        average['ap@1'] += res.get_average_precision_at1()
+        average['ap@3'] += res.get_average_precision_at3()
+        average['ap@5'] += res.get_average_precision_at5()
+        average['rec@1'] += res.get_recall_at1()
+        average['rec@3'] += res.get_recall_at3()
+        average['rec@5'] += res.get_recall_at5()
+    new_row = pd.Series({'model': model, 'rr': (average['rr'] / len(results)), 'ap@1': (average['ap@1'] / len(results)),
+                         'ap@3': (average['ap@3'] / len(results)), 'ap@5': (average['ap@5'] / len(results)),
+                         'rec@1': (average['rec@1'] / len(results)), 'rec@3': (average['rec@3'] / len(results)),
+                         'rec@5': (average['rec@5'] / len(results))})
+    return new_row.to_frame().T
