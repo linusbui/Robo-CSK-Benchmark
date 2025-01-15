f9568b6ab1d253a680722935473631351557442b
Janfiderheld <Janfitoe@web.de>
2025-01-15 16:19:56 +0100

    use GPT-4o instead of o1

--- utils/openai_prompter.py
+++ utils/openai_prompter.py
@@ -7,7 +7,7 @@ from utils.prompter import Prompter
 
 class OpenAIPrompter(Prompter):
     def __init__(self):
-        super().__init__("o1-2024-12-17")
+        super().__init__("gpt-4o-2024-08-06")
         json_text = json.load(open("../credentials.json"))
         self.client = OpenAI(
             api_key=json_text["api_key"],
@@ -20,6 +20,7 @@ class OpenAIPrompter(Prompter):
                 {"role": "system", "content": system_msg},
                 {"role": "user", "content": user_msg},
                 {"role": "user", "content": question},
-            ]
+            ],
+            temperature=self.temperature,
         )
         return response.choices[0].message.content
