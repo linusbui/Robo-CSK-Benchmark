f86302a190c65a23b45d0fd50d9bf9898bea566f
Janfiderheld <Janfitoe@web.de>
2025-01-22 13:38:46 +0100

    add prompting for tool substitution task

---
+++ tool-substitution/prompting/tool_sub_prompter.py
@@ -0,0 +1,86 @@
+import ast
+import json
+import random
+
+import pandas as pd
+
+from tool_sub_result import ToolSubstitutionResult
+from utils import OpenAIPrompter
+
+# prompters = [OpenAIPrompter(), LlamaPrompter(), GemmaPrompter()]
+prompters = [OpenAIPrompter()]
+system_msg = 'Imagine you are a robot in a household environment being confronted with a task a list of tools.'
+user_msg = 'What is the single tool from the given list that you think is most suitable to help you execute your task? Please only answer with the tool you chose.'
+
+aff_data = pd.read_csv('../affordance_data.csv', delimiter=',', on_bad_lines='skip')
+affordance_map = {}
+
+
+def preprocess_affordance_data():
+    for idx, row in aff_data.iterrows():
+        aff_dict = ast.literal_eval(row['Affordances'])
+        for idx2, aff_t in aff_dict.items():
+            aff = aff_t[0]
+            if aff not in affordance_map:
+                affordance_map[aff] = [row['Object']]
+            else:
+                affordance_map[aff].append(row['Object'])
+
+
+def prompt_all_models():
+    comb_result = pd.DataFrame(columns=['model', 'acc'])
+    for prompter in prompters:
+        with open("../affordance_task_map.json") as f:
+            task_data = json.load(f)
+        results = []
+        for affordance, task in task_data.items():
+            for t in task:
+                corr_tool = get_tool_for_affordance(affordance)
+                choices = get_unhelpful_tools_for_affordance(affordance) + [corr_tool]
+                random.shuffle(choices)
+                choices_string = ', '.join([c for c in choices])
+                question = f'Task:{t}\nTools: {choices_string}\nYour Choice:'
+                res = prompter.prompt_model(system_msg, user_msg, question)
+                tup = ToolSubstitutionResult(t, affordance, corr_tool, res, choices)
+                results.append(tup)
+        write_results_to_file(results, prompter.model_name)
+        comb_result = pd.concat([comb_result, calculate_average(results, prompter.model_name)],
+                                ignore_index=True)
+    comb_result.to_csv('../results/model_overview.csv', index=False)
+
+
+def get_tool_for_affordance(affordance: str) -> str:
+    if affordance not in affordance_map:
+        return affordance
+    return random.choice(affordance_map[affordance])
+
+
+def get_unhelpful_tools_for_affordance(affordance: str, amount=4) -> [str]:
+    choices = []
+    while len(choices) < amount:
+        pot_choice = aff_data.sample(n=1)
+        aff_dict = ast.literal_eval(pot_choice['Affordances'].iloc[0])
+        if affordance not in aff_dict:
+            choices.append(pot_choice['Object'].iloc[0])
+    return choices
+
+
+def write_results_to_file(results: [ToolSubstitutionResult], model: str):
+    dict_list = [re.to_dict() for re in results]
+    df = pd.DataFrame(dict_list)
+    df.to_csv(f'../results/{model}.csv', index=False)
+
+
+def calculate_average(results: [ToolSubstitutionResult], model: str):
+    average = {met: 0 for met in ['acc']}
+    for res in results:
+        if res.get_pred_correctness():
+            average['acc'] += 1
+    new_row = pd.Series(
+        {'model': model, 'acc': (average['acc'] / len(results))})
+    return new_row.to_frame().T
+
+
+if __name__ == "__main__":
+    preprocess_affordance_data()
+    prompt_all_models()
---
+++ tool-substitution/prompting/tool_sub_result.py
@@ -0,0 +1,35 @@
+class ToolSubstitutionResult:
+    def __init__(self, task: str, affordance: str, correct_tool: str, prediction: str, tool_choices: [str]):
+        self._task = task
+        self._aff = affordance
+        self._corr_tool = correct_tool.lower()
+        self._pred_tool = prediction.lower()
+        self._choices = tool_choices
+
+    def get_task(self) -> str:
+        return self._task
+
+    def get_affordance(self) -> str:
+        return self._aff
+
+    def get_predicted_tool(self) -> str:
+        return self._pred_tool
+
+    def get_correct_tool(self) -> str:
+        return self._corr_tool
+
+    def get_choices(self) -> [str]:
+        return self._choices
+
+    def get_pred_correctness(self) -> bool:
+        return self._pred_tool == self._corr_tool
+
+    def to_dict(self):
+        return {
+            'task': self.get_task(),
+            'affordance': self.get_affordance(),
+            'correct_tool': self.get_correct_tool(),
+            'predicted_tool': self.get_predicted_tool(),
+            'tool_choices': self.get_choices(),
+            'correct_prediction': self.get_pred_correctness(),
+        }
