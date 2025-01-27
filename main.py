05d2edd959fecf622dcc599bc3d37976e9fca5d8
Janfiderheld <Janfitoe@web.de>
2025-01-27 08:38:36 +0100

    prepare for Tidy Up task execution for Gemma

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from collaboration.prompting import collab_prompter
 from table_setting.prompting import table_setting_prompter
 from tidy_up.prompting import tidy_up_prompter
 from tool_usage.prompting import tool_use_prompter
-from utils import LlamaPrompter
+from utils import GemmaPrompter
 
 #prompters = [OpenAIPrompter(), LlamaPrompter(), GemmaPrompter()]
-prompters = [LlamaPrompter()]
+prompters = [GemmaPrompter()]
 
 if __name__ == "__main__":
-    table_setting_prompter.prompt_all_models(prompters)
-    print('Finished the Table Setting task')
-    collab_prompter.prompt_all_models(prompters)
-    print('Finished the Meta-Reasoning task')
-    tool_use_prompter.execute_prompting(prompters)
-    print('Finished the Tool Usage task')
+    #table_setting_prompter.prompt_all_models(prompters)
+    #print('Finished the Table Setting task')
+    #collab_prompter.prompt_all_models(prompters)
+    #print('Finished the Meta-Reasoning task')
+    #tool_use_prompter.execute_prompting(prompters)
+    #print('Finished the Tool Usage task')
     tidy_up_prompter.prompt_all_models(prompters)
     print('Finished the Tidy Up task')
