d33bdaf509ac151deccca9bf9343876331fb31ae
Janfiderheld <Janfitoe@web.de>
2025-07-16 19:17:20 +0200

    Update multi choice tidy up questions to use highest ranked location

--- tidy_up/prompting/multi_choice_generator.py
+++ tidy_up/prompting/multi_choice_generator.py
@@ -1,5 +1,6 @@
 import ast
 import random
+from typing import List
 
 import pandas as pd
 from tqdm import tqdm
@@ -14,17 +15,17 @@ def preprocess_location_data():
         locations.append(row['Location'])
 
 
-def turn_ranked_locations_into_list(corr_locs: str) -> [str]:
+def get_highest_ranked_locations(corr_locs: str) -> List[str]:
     parsed = ast.literal_eval(corr_locs)
-    locs = [v[0] for v in parsed.values()]
-    return locs
+    max_value = max(val[1] for val in parsed.values())
+    return [name for _, (name, value) in parsed.items() if value == max_value]
 
 
-def get_wrong_locations_for_object(corr_locs: str, amount=4) -> [str]:
+def get_wrong_locations_for_object(corr_locs: str, amount=4) -> List[str]:
     choices = []
     while len(choices) < amount:
         pot_choice = random.choice(locations)
-        locs = turn_ranked_locations_into_list(corr_locs)
+        locs = [v[0] for v in ast.literal_eval(corr_locs).values()]
         if pot_choice not in locs:
             choices.append(pot_choice)
     return choices
@@ -35,7 +36,7 @@ def create_multi_choice_questions():
     question_data = pd.DataFrame(columns=['Object', 'Correct_Location', 'Wrong_Locations'])
     for idx, row in tqdm(obj_data.iterrows(), 'Creating multiple choice questions'):
         obj = row['Object']
-        corr_locs = turn_ranked_locations_into_list(row['Locations'])
+        corr_locs = get_highest_ranked_locations(row['Locations'])
         chosen_corr_loc = random.choice(corr_locs)
         choices = get_wrong_locations_for_object(row['Locations'])
         new_row = pd.DataFrame([{
